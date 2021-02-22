69243db5d2fd7297f5887874bc396717d23f6179
willfynch <william.finzy@protonmail.com>
2021-02-22 14:56:55 +0100

    Check if first prompted string is twice as long as second prompted string

---
+++ exo1.js
@@ -0,0 +1,25 @@
+/*
+JavaScript exercise
+Asking user for 2 Strings
+Checking if first String is twice the length of second String's
+*/
+
+
+// ---- import scanf library --------
+var scanf = require('scanf');
+// -------------------------------
+
+// ----  user inputs -------------
+console.log("First chain : \n");
+let myChain1 = scanf("%S");
+console.log("Second chain : \n");
+let myChain2 = scanf("%S");
+// -------------------------------
+
+// checking if myChain1 is twice as long as myChain2 -----------------
+if(myChain1.length == myChain2.length*2){
+    console.log(`${myChain1} is twice as long as ${myChain2} !`);
+}else{
+    console.log(`${myChain1} is not twice as long as ${myChain2} !`);
+}
+// ---------------------------------------------------------------------
